c95a9762cea86ebc63744a08222c0c39f814bf8a
CJ <cjlazell@googlemail.com>
2011-08-05 19:16:09 -0500

    lots of tweaks, sublime is almost perfect now

--- git/blame.py
+++ git/blame.py
@@ -4,14 +4,14 @@ import sublime_plugin
 
 class BlameCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        if len(self.view.file_name()) > 0:  
+        if len(self.view.file_name()) > 0:
             folder_name, file_name = os.path.split(self.view.file_name())
             begin_line, begin_column = self.view.rowcol(self.view.sel()[0].begin())
             end_line, end_column = self.view.rowcol(self.view.sel()[0].end())
             begin_line = str(begin_line+1)
-            end_line = str(end_line)
+            end_line = str(end_line+1)
             lines = begin_line + ',' + end_line
-            self.view.window().run_command('exec', {'cmd': ['git', 'blame', '-L', lines, file_name], 'working_dir': folder_name})         
+            self.view.window().run_command('exec', {'cmd': ['git', 'blame', '-L', lines, file_name], 'working_dir': folder_name})
             sublime.status_message("git blame -L " + lines + " " + file_name)
 
     def is_enabled(self):
